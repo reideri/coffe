1d3e0975fe8a1676d1aff55a10994fde80ff5256
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-07 13:03:59 -0600

    Actualizacion dashboard

--- user/views/requires/headerDash.php
+++ user/views/requires/headerDash.php
@@ -54,7 +54,7 @@
             <div class="navbar-header">
                 <div class="top-left-part">
                     <!-- Logo -->
-                    <a class="logo" href="index.html">
+                    <a class="logo" href="">
                         <!-- Logo icon image, you can use font-icon also --><b>
                         <!--This is dark logo icon-->
                      </b>
