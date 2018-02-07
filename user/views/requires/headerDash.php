8ea07a1ef8a7095801fc519ced5342913ef85601
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-07 13:22:57 -0600

    Actualizacion menu

--- user/views/requires/headerDash.php
+++ user/views/requires/headerDash.php
@@ -54,10 +54,10 @@
             <div class="navbar-header">
                 <div class="top-left-part">
                     <!-- Logo -->
-                    <a class="logo" href="">
-                        <!-- Logo icon image, you can use font-icon also --><b>
-                        <!--This is dark logo icon-->
-                     </b>
+                    <a class="logo">
+                        <b>
+                           <img src="<?php echo RUTA.'img/menu.png' ?>"> <!--This is dark logo icon-->
+                        </b>
                         <!-- Logo text image you can use text also --><span class="hidden-xs">
                         <!--This is dark logo text--><img src="../plugins/images/admin-text.png" alt="home" class="dark-logo" /><!--This is light logo text--><img src="../plugins/images/admin-text-dark.png" alt="home" class="light-logo" />
                      </span> </a>
